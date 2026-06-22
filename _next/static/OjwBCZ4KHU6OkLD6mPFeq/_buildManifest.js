161c2639635a5aa05b974f990a21a47bb1d86631
Nishore-N <nagakalpanish2004@gmail.com>
2026-06-22 22:28:17 +0530

    Updates

--- _next/static/OjwBCZ4KHU6OkLD6mPFeq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/206-a572f70ef84fb41a.js","static/css/a6ddb88dd732d4c1.css","static/chunks/pages/index-3b8835baed084a20.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/mXD6lzqHD5cgyKJyI4txv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/206-a572f70ef84fb41a.js","static/css/a6ddb88dd732d4c1.css","static/chunks/pages/index-915669e9099f5baf.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
